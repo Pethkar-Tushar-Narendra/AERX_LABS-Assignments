57c6cc04027c6ed55b0826630f14b0bc8568c8ec
Pethkar-Tushar-Narendra <108254800+Pethkar-Tushar-Narendra@users.noreply.github.com>
2022-11-10 22:24:23 +0530

    Final commit

--- interview_assignment/aerx_labs/Assignment2Thread.java
+++ interview_assignment/aerx_labs/Assignment2Thread.java
@@ -7,17 +7,28 @@ public class Assignment2Thread {
 
 		int[] array = {19,20,21,22,23,24,28,26,27,29,30,31,32,33,25,35,36,37,1,40,41,42,43,44,45,2,3,4,5,6,7,8,57,34,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,58,9,10,11,12,13,14,15,16,17,18,38,39,46,47,48,49,50,51,52,53,54,55,56,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
 		
-		find_missing_number(array);
+		Assignment2Thread assignment2Thread = new Assignment2Thread();
+		assignment2Thread.find_missing_number(array);
 	}
-	private static void find_missing_number(int[] array) throws InterruptedException {	
-		//for unsorted Array
-		//array size less than 99 or more Array not upto mark exception
+	private void find_missing_number(int[] array) throws InterruptedException {
+		//if array is more or less in number give invalid array
+		try {
+			if(array.length!=99)
+				throw new InvalidArrayException("Invalid Array Size of array is not 99 but it is more or less.");
+			} catch (InvalidArrayException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e.getMessage());
+				System.out.println();
+				return;
+			}
+		
+		//if array is valid array
 		Thread t1 = new Thread(()->{
 			int count=0;
 			for(int i=1;i<=25;i++) {count=0;
 				for(int j=0;j<array.length;j++) {
 					if(i==array[j]) {count++;break;}
-				}if(count==0) {System.out.println("Missing Integer is "+i);return;}
+				}if(count==0) {System.out.println("Missing Number is "+i);return;}
 			}
 		});
 		Thread t2 = new Thread(()->{
@@ -25,7 +36,7 @@ public class Assignment2Thread {
 			for(int i=26;i<=50;i++) {count=0;
 			for(int j=0;j<array.length;j++) {
 				if(i==array[j]) {count++;break;}
-			}if(count==0) {System.out.println("Missing Integer is "+i);return;}
+			}if(count==0) {System.out.println("Missing Number is "+i);return;}
 		}
 		});
 		Thread t3 = new Thread(()->{
@@ -33,7 +44,7 @@ public class Assignment2Thread {
 			for(int i=51;i<=75;i++) {count=0;
 				for(int j=0;j<array.length;j++) {
 					if(i==array[j]) {count++;break;}
-				}if(count==0) {System.out.println("Missing Integer is "+i);return;}
+				}if(count==0) {System.out.println("Missing Number is "+i);return;}
 			}
 		});
 		Thread t4 = new Thread(()->{
@@ -41,7 +52,7 @@ public class Assignment2Thread {
 			for(int i=76;i<=100;i++) {count=0;
 				for(int j=0;j<array.length;j++) {
 					if(i==array[j]) {count++;break;}
-				}if(count==0) {System.out.println("Missing Integer is "+i);return;}
+				}if(count==0) {System.out.println("Missing Number is "+i);return;}
 			}
 		});
 		t1.start();
--- interview_assignment/aerx_labs/EmptyListException.java
+++ interview_assignment/aerx_labs/EmptyListException.java
@@ -1,7 +1,6 @@
 package interview_assignment.aerx_labs;
 
 public class EmptyListException extends Exception{
-
 	EmptyListException(String s){
 		super(s);
 	}
