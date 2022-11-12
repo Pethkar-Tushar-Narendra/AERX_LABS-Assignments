30fa6f71ff8c7176cbbc984ae65cda000bf6e50a
Pethkar-Tushar-Narendra <108254800+Pethkar-Tushar-Narendra@users.noreply.github.com>
2022-11-12 22:53:56 +0530

    Delete interview_assignment/aerx_labs directory

--- interview_assignment/aerx_labs/EmptyListException.java
+++
@@ -1,7 +0,0 @@
-package interview_assignment.aerx_labs;
-
-public class EmptyListException extends Exception{
-	EmptyListException(String s){
-		super(s);
-	}
-}
--- interview_assignment/aerx_labs/InvalidArrayException.java
+++
@@ -1,7 +0,0 @@
-package interview_assignment.aerx_labs;
-
-public class InvalidArrayException extends Exception {
-	InvalidArrayException(String s){
-		super(s);
-	}
-}
